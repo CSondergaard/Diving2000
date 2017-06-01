f3a3f26e79eeaf54cc5a86fcb642a42bfa5a447a
Casper Søndergaard <casp4807@hotmail.com>
2017-06-01 10:24:03 +0200

    unittest

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -48,18 +48,18 @@ namespace UnitTest
 
         public void CreateEquipment()
         {
-            Equip.Add(new Equipment(1, "Dragt", 2, DateTime.Now));
+            Equip.Add(new Equipment(1, 2, DateTime.Now));
             Equipment equips = Equip.GetById(1);
-            Assert.AreEqual(equips._name, "Dragt");
+            Assert.AreEqual(equips._id, 1);
         }
 
         [TestMethod]
 
         public void EditEquipment()
         {
-            Equip.Edit(new Equipment(1, "Hansker", 2, DateTime.Today));
+            Equip.Edit(new Equipment(1, 3, DateTime.Now));
             Equipment equips = Equip.GetById(1);
-            Assert.AreEqual(equips._name, "Hansker");
+            Assert.AreEqual(equips._catId, 3);
         }
 
         [TestMethod]
@@ -94,12 +94,12 @@ namespace UnitTest
             dic2.Add("Tykkelse7", "8mm");
 
 
-            Equip.Add(new Equipment(1, "Dragt",  2, DateTime.Now, dic));
-            Equip.Add(new Equipment(2, "Dragt",  2, DateTime.Now, dic));
-            Equip.Add(new Equipment(3, "Dragt",  2, DateTime.Now, dic));
+            Equip.Add(new Equipment(1, 2, DateTime.Now, dic));
+            Equip.Add(new Equipment(2, 2, DateTime.Now, dic));
+            Equip.Add(new Equipment(3, 2, DateTime.Now, dic));
             for (int i = 4; i < 250000; i++)
             {
-                Equip.Add(new Equipment(i, "Dragt", 2, DateTime.Now, dic2));
+                Equip.Add(new Equipment(i, 2, DateTime.Now, dic2));
             }
 
 
@@ -117,7 +117,7 @@ namespace UnitTest
         [TestMethod]
         public void CreateBooking()
         {
-            EquipList.Add(new Equipment(1, "Dragt", 2, DateTime.Now));
+            EquipList.Add(new Equipment(1, 2, DateTime.Now));
             Book.Add(new Booking(1, EquipList, DateTime.Now, DateTime.Today, "41412622", true));
             Booking bk = Book.GetById(1);
             Assert.AreEqual(bk._phone, "41412622");
@@ -134,7 +134,7 @@ namespace UnitTest
         public void AddEquipmentToCurrentBooking()
         {
             Booking bk = Book.GetById(1);
-            Equipment eq = new Equipment(2, "Handske", 1);
+            Equipment eq = new Equipment(2, 1);
 
             int nr = bk._equipment.Count;
 
@@ -149,7 +149,7 @@ namespace UnitTest
         {
 
             Booking bk = Book.GetById(1);
-            Equipment eq = new Equipment(2, "Handske",  1);
+            Equipment eq = new Equipment(2,  1);
 
             int nr = bk._equipment.Count;
 
