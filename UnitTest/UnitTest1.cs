11eb03b29619c14967b89aa6a34ad61fa56ae031
Casper Søndergaard <casp4807@hotmail.com>
2017-06-01 21:02:49 +0200

    unittesr

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -32,14 +32,6 @@ namespace UnitTest
         }
 
         [TestMethod]
-        public void CreateCategoryFail()
-        {
-            cat.Add(new Category(2, "test", "billede.jpeg", true, 2));
-            Category cats = cat.GetById(2);
-            Assert.AreNotEqual(cats._name, 22);
-        }
-
-        [TestMethod]
         public void EditCategory()
         {
             cat.Edit(new Category(1, "OtherName", "nytbillede.jpg", true, 2));
@@ -49,26 +41,10 @@ namespace UnitTest
         }
 
         [TestMethod]
-        public void EditCategoryFail()
-        {
-            cat.Edit(new Category(2, "OtherName", "nytbillede.jpg", true, 2));
-            Category cats = cat.GetById(2);
-            Assert.AreNotEqual(cats._name, "OtherName!");
-            Assert.AreNotEqual(cats._thumbnail, 222);
-        }
-
-        [TestMethod]
         public void DeleteCategory()
         {
             cat.DeleteById(1);
             Assert.IsTrue(cat.GetById(1) == null);
-        }
-
-        [TestMethod]
-        public void DeleteCategoryFail()
-        {
-            cat.DeleteById(1);
-            Assert.AreNotEqual(cat.GetById(1), 2);
         }
 
         [TestMethod]
@@ -83,17 +59,6 @@ namespace UnitTest
         }
 
         [TestMethod]
-        public void AddCategoryValueFail()
-        {
-            Property propper = new Property(1, "Value");
-            cat.Add(new Category(1, "AddValue", "doen'tmatter.jpeg", true, 12));
-            cat.AddValue(1, propper);
-            Category cats = cat.GetById(1);
-            Property pro = cats._values.Find(x => x._id == 1);
-            Assert.AreNotEqual(pro, 1);
-        }
-
-        [TestMethod]
         public void GetByName()
         {
 
@@ -102,25 +67,7 @@ namespace UnitTest
             Assert.AreEqual(cats._name, "FindMe");
         }
 
-        [TestMethod]
-        public void GetByNameFail()
-        {
-
-            cat.Add(new Category(1, "FindMe", "doesn'tmatter.jpeg", true, 12));
-            Category cats = cat.GetByName("FindMe");
-            Assert.AreNotEqual(2, cats._name);
-        }
-
         //Equitment
-        [TestMethod]
-
-        public void CreateEquipmentFail()
-        {
-            Equip.Add(new Equipment(2, 2, DateTime.Now));
-            Equipment equips = Equip.GetById(2);
-            Assert.AreNotEqual(equips._id, "!");
-        }
-
         [TestMethod]
 
         public void CreateEquipment()
@@ -141,27 +88,12 @@ namespace UnitTest
 
         [TestMethod]
 
-        public void EditEquipmentFail()
-        {
-            Equip.Edit(new Equipment(1, 3, DateTime.Now));
-            Equipment equips = Equip.GetById(1);
-            Assert.AreNotEqual(equips._catId, "fisk");
-        }
-
-        [TestMethod]
-
         public void DeleteEquipment()
         {
             Equip.DeleteById(1);
             Assert.IsTrue(Equip.GetById(1) == null);
         }
-        [TestMethod]
 
-        public void DeleteEquipmentFail()
-        {
-            Equip.DeleteById(1);
-            Assert.AreNotEqual(Equip.GetById(1), 1);
-        }
         [TestMethod]
         public void DeleteEquipment2()
         {
@@ -211,49 +143,6 @@ namespace UnitTest
 
         }
 
-        [TestMethod]
-        public void SearchEquipmentFail()
-        {
-
-            Dictionary<string, string> dic = new Dictionary<string, string>();
-            dic.Add("Str", "32");
-            dic.Add("Tykkelse", "5mm");
-            dic.Add("Tykkelse2", "4mm");
-            dic.Add("Tykkelse3", "3mm");
-            dic.Add("Tykkelse4", "2mm");
-            dic.Add("Tykkelse5", "1mm");
-            dic.Add("Tykkelse6", "6mm");
-            dic.Add("Tykkelse7", "8mm");
-            Dictionary<string, string> dic2 = new Dictionary<string, string>();
-            dic2.Add("Str", "34");
-            dic2.Add("Tykkelse", "4mm");
-            dic2.Add("Tykkelse2", "4mm");
-            dic2.Add("Tykkelse3", "3mm");
-            dic2.Add("Tykkelse4", "2mm");
-            dic2.Add("Tykkelse5", "1mm");
-            dic2.Add("Tykkelse6", "6mm");
-            dic2.Add("Tykkelse7", "8mm");
-
-
-            Equip.Add(new Equipment(1, 2, DateTime.Now, dic));
-            Equip.Add(new Equipment(2, 2, DateTime.Now, dic));
-            Equip.Add(new Equipment(3, 2, DateTime.Now, dic));
-            for (int i = 4; i < 250000; i++)
-            {
-                Equip.Add(new Equipment(i, 2, DateTime.Now, dic2));
-            }
-
-
-
-            List<Equipment> eq = Equip.GetAllEquipments();
-            eq = Equip.GetEquipmentsFromCategory(2, eq);
-            eq = eqSearch.SearchEquipment(dic2, eq);
-            Assert.AreNotEqual(24996, eq.Count);
-
-
-        }
-
-
         //Booking
 
         [TestMethod]
@@ -265,14 +154,6 @@ namespace UnitTest
             Assert.AreEqual(bk._phone, "41412622");
         }
 
-        [TestMethod]
-        public void CreateBookingFail()
-        {
-            EquipList.Add(new Equipment(2, 2, DateTime.Now));
-            Book.Add(new Logic.Booking(2, EquipList, DateTime.Now, DateTime.Today, "HerKommerPipi", true));
-            Logic.Booking bk = Book.GetById(2);
-            Assert.AreNotEqual(bk._phone, "41412622");
-        }
 
         [TestMethod]
         public void AddEquipmentToCurrentBooking()
@@ -286,20 +167,6 @@ namespace UnitTest
             bk = Book.GetById(1);
             int newnr = bk._equipment.Count;
             Assert.AreEqual(nr + 1, newnr);
-        }
-
-        [TestMethod]
-        public void AddEquipmentToCurrentBookingFail()
-        {
-            Logic.Booking bk = Book.GetById(1);
-            Equipment eq = new Equipment(2, 1);
-
-            int nr = bk._equipment.Count;
-
-            Book.AddEquipmentToBooking(eq, bk);
-            bk = Book.GetById(1);
-            int newnr = bk._equipment.Count;
-            Assert.AreNotEqual(nr + "heathrow", newnr);
         }
 
         [TestMethod]
@@ -325,16 +192,6 @@ namespace UnitTest
             Logic.Booking bk = Book.GetById(5);
             Book.GetByPhone("21212121");
             Assert.AreEqual("21212121", bk._phone);
-        }
-
-        [TestMethod]
-
-        public void GetByPhoneFail()
-        {
-            Book.Add(new Logic.Booking(5, EquipList, DateTime.Now, DateTime.Today, "21212121", true));
-            Logic.Booking bk = Book.GetById(5);
-            Book.GetByPhone("21212121");
-            Assert.AreNotEqual(21212121, bk._phone);
         }
     }
 }
