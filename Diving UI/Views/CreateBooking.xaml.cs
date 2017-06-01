4e9d3a55bac3f854a92a87bb6c50654be436823a
Casper Søndergaard <casp4807@hotmail.com>
2017-06-01 23:23:00 +0200

    ok

--- Diving UI/Views/CreateBooking.xaml.cs
+++ Diving UI/Views/CreateBooking.xaml.cs
@@ -15,6 +15,7 @@ using Logic;
 using Logic.Repository;
 using Diving_UI.Model;
 using Logic.Data;
+using System.Text.RegularExpressions;
 
 namespace Diving_UI.Views
 {
@@ -35,6 +36,17 @@ namespace Diving_UI.Views
             SetDate();
             CreateEquipmentList();
         }
+        private static bool IsTextAllowed(string text)
+        {
+            Regex regex = new Regex("[^0-9]+"); //regex that matches disallowed text
+            return !regex.IsMatch(text);
+        }
+
+        private void Phone_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
 
         private void SetDate()
         {
